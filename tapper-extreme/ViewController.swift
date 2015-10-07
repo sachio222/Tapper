1b3fa217e9274846037f270d9d2ce854cb37d37f
bpUX <brainpowerux@gmail.com>
2015-10-07 16:09:24 -0400

    commiting project

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -9,17 +9,83 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Properties
+    var maxTaps = 0
+    var currentTaps = 0
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapLbl: UILabel!
+    
+    @IBAction func onCoinTapped (sender: UIButton!) {
+        
+        if currentTaps <= maxTaps {
+            currentTaps++
+            updateTapLbl()
+        }
+        
+        if isGameOver(){
+            restartGame()
+        }
+        
     }
+    
+    @IBAction func onPlayButtonPressed(sender: UIButton!) {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            
+            // Hide visible UI
+            logoImg.hidden = true
+            howManyTapsTxt.hidden = true
+            playBtn.hidden = true
+            
+            // Show coin and label
+            tapBtn.hidden = false
+            tapLbl.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            updateTapLbl()
+            
+        }
     }
-
-
+    func restartGame(){
+        
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        
+        // Show visible UI
+        logoImg.hidden = false
+        howManyTapsTxt.hidden = false
+        playBtn.hidden = false
+        
+        // hide coin and label
+        tapBtn.hidden = true
+        tapLbl.hidden = true
+        
+        
+    }
+    
+    func isGameOver() -> Bool{
+        
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    // updating label
+    func updateTapLbl(){
+        tapLbl.text = "\(currentTaps) Taps"
+    }
+    
 }
 
